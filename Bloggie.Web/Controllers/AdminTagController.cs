8584c4ba50872a113c9ec1acb657c2426aedb0b4
Mahmoud Ahmed <hoda1892003@gmail.com>
2025-02-21 23:42:25 +0200

    Create Crud Operation

---
+++ Bloggie.Web/Controllers/AdminTagController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Bloggie.Web.Data;
+using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Models.Domain;
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagController : Controller
+    {
+        private readonly BloggieDbContext _context;
+
+
+
+        public AdminTagController(BloggieDbContext context) 
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult SubmitTag(AddReqestTag _add)
+        {
+            var tag = new Tag {
+                Name = _add.Name,
+                DisplayName = _add.DisplayName,
+            };
+            _context.Tags.Add(tag);
+            _context.SaveChanges();
+            return RedirectToAction("List");
+        }
+        public IActionResult List() {
+            var tag = _context.Tags.ToList();
+            return View(tag);
+        }
+    }
+}
---
+++ Bloggie.Web/Models/ViewModels/AddReqestTag.cs
@@ -0,0 +1,8 @@
+﻿namespace Bloggie.Web.Models.ViewModels
+{
+    public class AddReqestTag
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
