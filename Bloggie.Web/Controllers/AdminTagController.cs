a13a5f62d263b50dbb004018099fb023577913e7
Mahmoud Ahmed <hoda1892003@gmail.com>
2025-02-22 14:06:06 +0200

    Tag Crud Operations is completed

--- Bloggie.Web/Controllers/AdminTagController.cs
+++ Bloggie.Web/Controllers/AdminTagController.cs
@@ -7,20 +7,18 @@ namespace Bloggie.Web.Controllers
     public class AdminTagController : Controller
     {
         private readonly BloggieDbContext _context;
-
-
-
-        public AdminTagController(BloggieDbContext context) 
+        public AdminTagController(BloggieDbContext context)
         {
             _context = context;
         }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
         }
-        [HttpPost]
 
+        [HttpPost]
         public IActionResult SubmitTag(AddReqestTag _add)
         {
             var tag = new Tag {
@@ -31,9 +29,67 @@ namespace Bloggie.Web.Controllers
             _context.SaveChanges();
             return RedirectToAction("List");
         }
+
+        [HttpGet]
         public IActionResult List() {
             var tag = _context.Tags.ToList();
             return View(tag);
         }
+
+        [HttpGet]
+        public IActionResult Edit(Guid id) {
+            var tag = _context.Tags.FirstOrDefault(e => e.Id == id);
+            if (tag != null)
+            {
+                var edittag = new EditTagRequest{
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    DisplayName = tag.DisplayName,
+                 };
+                return View(edittag);
+            }
+            return View(null);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditTagRequest _editTagRequest) {
+
+            var tag = new Tag {
+                Id = _editTagRequest.Id,
+                Name = _editTagRequest.Name,
+                DisplayName= _editTagRequest.DisplayName,
+            };
+            var existtag = _context.Tags.FirstOrDefault(e=> e.Id == tag.Id);
+
+            if (existtag != null) {
+
+                existtag.Name = tag.Name;
+                existtag.DisplayName = tag.DisplayName;
+
+                // save changes
+                _context.SaveChanges();
+                //show success notify
+                return RedirectToAction("Edit", new { id = _editTagRequest.Id });
+            }
+            // show error notify
+            return RedirectToAction("Edit", new { id = _editTagRequest.Id });
+        }
+        
+        [HttpPost]
+
+        public IActionResult Delete(EditTagRequest _editTagRequest) {
+
+            var tag = _context.Tags.Find(_editTagRequest.Id);
+            if (tag != null) {
+                _context.Tags.Remove(tag);
+                _context.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return RedirectToAction("Edit", new { id = _editTagRequest.Id });
+        }
+
+
+
+
     }
 }
